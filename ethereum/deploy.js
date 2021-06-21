efec10b0f1b91a66a30c934e91e213b5e89b07fd
David Killen <david.killen@outlook.com>
2021-06-21 20:34:49 +1000

    Added contracts and tests

---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const { WALLET_MNEMONIC, TEST_NETWORK_URL } = require("./secrets");
+
+const provider = new HDWalletProvider(WALLET_MNEMONIC, TEST_NETWORK_URL);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
